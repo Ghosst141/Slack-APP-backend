6c14a3e12d505a221f93e3b7f167042c7b7c51dc
Ghosst141 <ommyr1234@gmail.com>
2025-07-02 18:54:30 +0530

    Initial backend commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express,{Request,Response} from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes';
+import messageRoutes from './routes/messageRoutes';
+import scheduler from './scheduler/scheduler';
+import db from './db/database';
+import Database from 'better-sqlite3';
+const dbs = new Database('data.db');
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors({
+  origin: 'http://localhost:5173', // or whatever port your frontend runs on
+  credentials: true,
+}));
+app.use(express.json());
+app.get('/hi', (req:Request, res:Response) => {
+  res.json({ message: 'Hello from the backend!' });
+})
+
+app.get('/delaccess', (req:Request, res:Response) => {
+  dbs.prepare(`update tokens 
+    set access_token = 'dadsadasaca'
+    `).run();
+  res.json({ message: 'Hello from the aceess change!' });
+})
+
+app.use('/auth', authRoutes);
+app.use('/messages', messageRoutes);
+
+db.initialize();
+scheduler.initialize();
+
+app.listen(PORT, () => console.log(`Backend server running on port ${PORT}`));
